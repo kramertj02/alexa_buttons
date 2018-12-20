3e99f1a3a39d20dd4ede1f62ae4f43f4c9b52955
Tim Kramer <kramertj@localhost.localdomain>
2018-12-20 13:06:07 -0500

    Testing

--- key_intent.py
+++ key_intent.py
@@ -1,11 +1,10 @@
-import requests
 from alexa_skill.intents import BaseIntents
 
 
 class JokeIntents(BaseIntents):
     @property
     def mapper(self):
-        return {'button_buzzer': self.button_buzzer}
+        return {'buzzer': self.button_buzzer}
 
     def button_buzzer(self):
         return self.response('Kyle sucks'), True
