45cae53565283cfd223cd2c8a3252e2ae908070f
Tim Kramer <kramertj@localhost.localdomain>
2018-12-20 12:47:37 -0500

    Hahah

--- key_intent.py
+++ key_intent.py
@@ -7,9 +7,4 @@ class JokeIntents(BaseIntents):
     def mapper(self):
         return {'random_joke': self.random_joke}
 
-    def random_joke(self):
-        joke_response = requests.get('http://api.icndb.com/jokes/random')
-        if joke_response.status_code != 200:
-            return self.response('Sorry, I did not find any joke. Please try again'), False
-
-        return self.response(joke_response['value']['joke']), True
+    def random_joke(self):return self.response('Kyle sucks'), True
