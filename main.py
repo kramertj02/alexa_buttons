58c033fd2fb068ac0a8fb2bf234e1961d4063a12
Tim Kramer <kramertj@localhost.localdomain>
2018-12-20 12:41:20 -0500

    Changing exit messages

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from alexa_skill.intents import BuildInIntents
 buildin_intents = BuildInIntents(
     help_message='Say to Alexa: "tell me a joke"',
     not_handled_message="Sorry, I don't understand you. Could you repeat?",
-    stop_message='stop',
-    cancel_message='cancel',
+    stop_message='Thanks for playing!',
+    cancel_message='Closing button buzzer skill...',
 )
